fe6197f66ce88983047a4b99a677522e39b78e4d
pbain63 <prodipbain63@gmail.com>
2025-06-12 21:16:56 +0600

    Verify users login from email and password by using bcrypt

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 // server/routes/auth.js
 const express = require("express");
-const bcrypt = require("bcryptjs");
+// const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const pool = require("../db");
 require("dotenv").config();
@@ -29,4 +30,37 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// POST /api/auth/login
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find user by email
+    const userResult = await pool.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+    const user = userResult.rows[0];
+
+    if (!user) {
+      return res.status(401).json({ error: "Invalid credentials (email)" });
+    }
+
+    // Compare plain password with hashed one
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ error: "Invalid credentials (password)" });
+    }
+
+    // Create JWT token
+    const token = jwt.sign(
+      { id: user.id, role: user.role },
+      process.env.JWT_SECRET
+    );
+    res.json({ token });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: "Server error" });
+  }
+});
 module.exports = router;
