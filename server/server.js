400b44254654bf1d105ba25c6c5b8c3a50455e47
pbain63 <prodipbain63@gmail.com>
2025-06-12 13:14:50 +0600

    Update server routeto respond to /api

--- server/server.js
+++ server/server.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
+// server/server.js
+app.get("/api", (req, res) => {
   res.send("API is running!");
 });
 
