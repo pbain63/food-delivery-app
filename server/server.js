eec22f3499c7d6c57753d58e5af7ff9e76c32a93
pbain63 <prodipbain63@gmail.com>
2025-06-12 14:41:22 +0600

    Update with authRoutes

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const authRoutes = require("./routes/auth");
 require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// server/server.js
+app.use("/api/auth", authRoutes);
+
 app.get("/api", (req, res) => {
   res.send("API is running!");
 });
