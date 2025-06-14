dbdac5235dc3a0cc9a9b8c5dcebcfc803e92bcc3
pbain63 <prodipbain63@gmail.com>
2025-06-14 13:43:34 +0600

    Update for auto add mill card to page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
+import { useEffect, useState } from "react";
 import AddMealForm from "./components/AddMealForm";
-import MealsPage from "./components/MealsPage";
+import MealsPage from "./pages/MealsPage";
 
 function App() {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/meals")
+      .then((res) => res.json())
+      .then((data) => setMeals(data.meals))
+      .catch((err) => console.error("Failed to fetch meals:", err));
+  }, []);
+
   return (
     <div style={{ textAlign: "center", marginTop: "2rem" }}>
-      <AddMealForm />
+      <AddMealForm setMeals={setMeals} />
       <hr style={{ margin: "2rem 0" }} />
-      <MealsPage />
+      <MealsPage meals={meals} />
     </div>
   );
 }
