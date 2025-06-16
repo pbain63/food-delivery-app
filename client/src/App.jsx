3d96e109a05380d6296751bee932bf0ec51027ba
pbain63 <prodipbain63@gmail.com>
2025-06-16 22:14:41 +0600

    Import HomePage, use shared navbar component, Add Home page route inside Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,24 @@
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import MealsPage from "./pages/MealsPage";
-import HomePage from "./pages/HomePage";
-import ProviderDashboard from "./pages/ProviderDashboard";
-import { useAuth } from "./contexts/AuthContext";
+import HomePage from "./pages/HomePage"; // Added HomePage
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  const { user, logout } = useAuth();
-
   return (
     <div>
-      <Navbar />
-
+      <Navbar /> {/*  Use shared Navbar component */}
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} /> {/* Add Home page route */}
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route
           path="/meals"
           element={
-            <ProtectedRoute allowedRoles={["customer", "delivery"]}>
+            <ProtectedRoute>
               <MealsPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/provider-dashboard"
-          element={
-            <ProtectedRoute allowedRoles={["provider"]}>
-              <ProviderDashboard />
             </ProtectedRoute>
           }
         />
