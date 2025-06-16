95e54d56761c9a17d9eb8636d2961e3842c63fc4
pbain63 <prodipbain63@gmail.com>
2025-06-16 09:42:28 +0600

    Create DeliveryDashboard.jsx and add welcome text

---
+++ client/src/pages/DeliveryDashboard.jsx
@@ -0,0 +1,11 @@
+// src/pages/DeliveryDashboard.jsx
+function DeliveryDashboard() {
+  return (
+    <div>
+      <h2>Delivery Dashboard</h2>
+      <p>Welcome! Manage your delivery orders here.</p>
+    </div>
+  );
+}
+
+export default DeliveryDashboard;
--- client/src/pages/ProviderDashboard.jsx
+++ client/src/pages/ProviderDashboard.jsx
@@ -1,11 +1,11 @@
 // src/pages/ProviderDashboard.jsx
 function ProviderDashboard() {
-    return (
-      <div>
-        <h2>Provider Dashboard</h2>
-        <p>Welcome! Manage your meals here.</p>
-      </div>
-    );
-  }
-  
-  export default ProviderDashboard;
+  return (
+    <div>
+      <h2>Provider Dashboard</h2>
+      <p>Welcome! Manage your meals here.</p>
+    </div>
+  );
+}
+
+export default ProviderDashboard;
