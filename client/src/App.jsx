c95b6814d9f9b414fcb1b79f8f54e9b601b783ce
pbain63 <prodipbain63@gmail.com>
2025-06-16 09:51:11 +0600

    Register Routes and include new routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,22 @@
 // client/src/App.jsx
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import MealsPage from "./pages/MealsPage";
 import HomePage from "./pages/HomePage";
+import CustomerDashboard from "./pages/CustomerDashboard";
+import ProviderDashboard from "./pages/ProviderDashboard";
+import DeliveryDashboard from "./pages/DeliveryDashboard";
 import { useAuth } from "./contexts/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  const { user, logout } = useAuth(); // used in Navbar now
+  const { user, logout } = useAuth();
 
   return (
     <div>
       <Navbar />
-
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<RegisterPage />} />
@@ -24,6 +26,30 @@ function App() {
           element={
             <ProtectedRoute>
               <MealsPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/customer"
+          element={
+            <ProtectedRoute>
+              <CustomerDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/provider"
+          element={
+            <ProtectedRoute>
+              <ProviderDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/delivery"
+          element={
+            <ProtectedRoute>
+              <DeliveryDashboard />
             </ProtectedRoute>
           }
         />
