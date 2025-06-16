2060a2a4c93b634573811ca5e3a9b7b12822034d
pbain63 <prodipbain63@gmail.com>
2025-06-16 09:38:36 +0600

    Create CustomerDashboard.jsx and add welcome text

---
+++ client/src/pages/CustomerDashboard.jsx
@@ -0,0 +1,11 @@
+// src/pages/CustomerDashboard.jsx
+function CustomerDashboard() {
+  return (
+    <div>
+      <h2>Customer Dashboard</h2>
+      <p>Welcome! Browse and order delicious meals.</p>
+    </div>
+  );
+}
+
+export default CustomerDashboard;
