49b844dc8ae83b4678f543f7e2c197c1bc068dac
pbain63 <prodipbain63@gmail.com>
2025-06-16 20:05:49 +0600

    Update code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,78 +1,42 @@
-// client/src/App.jsx
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import MealsPage from "./pages/MealsPage";
 import HomePage from "./pages/HomePage";
-import CustomerDashboard from "./pages/CustomerDashboard";
 import ProviderDashboard from "./pages/ProviderDashboard";
-import DeliveryDashboard from "./pages/DeliveryDashboard";
+import { useAuth } from "./contexts/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
-import RedirectIfLoggedIn from "./components/RedirectIfLoggedIn";
 
 function App() {
+  const { user, logout } = useAuth();
+
   return (
-    <>
+    <div>
       <Navbar />
 
       <Routes>
         <Route path="/" element={<HomePage />} />
-
-        {/* ✅ Wrapped with redirect logic */}
-        <Route
-          path="/register"
-          element={
-            <RedirectIfLoggedIn>
-              <RegisterPage />
-            </RedirectIfLoggedIn>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <RedirectIfLoggedIn>
-              <LoginPage />
-            </RedirectIfLoggedIn>
-          }
-        />
-
-        {/* ✅ Role Dashboards */}
-        <Route
-          path="/dashboard/customer"
-          element={
-            <ProtectedRoute>
-              <CustomerDashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/dashboard/provider"
-          element={
-            <ProtectedRoute>
-              <ProviderDashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/dashboard/delivery"
-          element={
-            <ProtectedRoute>
-              <DeliveryDashboard />
-            </ProtectedRoute>
-          }
-        />
-
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route
           path="/meals"
           element={
-            <ProtectedRoute>
+            <ProtectedRoute allowedRoles={["customer", "delivery"]}>
               <MealsPage />
             </ProtectedRoute>
           }
         />
+        <Route
+          path="/provider-dashboard"
+          element={
+            <ProtectedRoute allowedRoles={["provider"]}>
+              <ProviderDashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
-    </>
+    </div>
   );
 }
 
