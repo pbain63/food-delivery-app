b59b34f4f4c07c0ee233bdda62f0eab80c2b5761
pbain63 <prodipbain63@gmail.com>
2025-06-17 12:49:16 +0600

    Add OrderPage route and updated code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,22 @@
-import { Routes, Route } from "react-router-dom";
+// client/src/App.jsx
+import { Routes, Route, Link } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import MealsPage from "./pages/MealsPage";
-import HomePage from "./pages/HomePage"; // Added HomePage
+import OrdersPage from "./pages/OrdersPage";
+import HomePage from "./pages/HomePage";
+import { useAuth } from "./contexts/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
+  const { user, logout } = useAuth();
+
   return (
     <div>
-      <Navbar /> {/*  Use shared Navbar component */}
+      <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} /> {/* Add Home page route */}
+        <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route
@@ -19,6 +24,14 @@ function App() {
           element={
             <ProtectedRoute>
               <MealsPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/orders"
+          element={
+            <ProtectedRoute>
+              <OrdersPage />
             </ProtectedRoute>
           }
         />
